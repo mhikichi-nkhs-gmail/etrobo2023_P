a46dc47c7ff0e3cf42e2b820a1649d4b4c8c8272
sato-nkhshc411033 <nkhshc411033@g.neec.ac.jp>
2023-10-31 16:11:09 +0900

    Update SpeedSectionManager.h

--- section/SpeedSectionManager.h
+++ section/SpeedSectionManager.h
@@ -6,7 +6,28 @@ class SpeedSectionManager : public SectionManager {
     public:
         SpeedSectionManager();
         
+/*
+走法
+WALKER{前進する強さ,曲がる強さ}
+TRACER{前進する強さ,輝度値(-0.25から-0.35くらい),P値,I値,d値,0,0}
+TEEL{プラスの値->しっぽを伸ばす},{マイナスの値->しっぽをしまう}
+DEFOLTARMCHANGE{目標のアームの角度}
+VIRTUALCURVE{プラス値->時計周り    ,前進の強さ,P値,I値,D値}
+             マイナス値->逆時計周り
+VIRTUALSTRAIGHT{基本0でいいよ,前進の強さ,P値,I値,D値}
+TURNWALKER{その場旋回の強さ,P値,I値,D値}
 
+判定
+LENGTH{長さ}
+COLOR{色の値,さい度(だいたい 0.5)}
+BLOCKCOLOR{赤ブロックの値,青ブロックの値,さい度,アームを止める時間}
+TIME{時間}
+TURNANGLE{目標の旋回角度}
+TEELANGLE{伸ばすとき->4700}
+          しまうとき->-4700
+BRIGHTNESS{輝度値(だいたい-0.3),0->黒判定}
+                               1->白判定
+*/
     protected:
 
     private:
