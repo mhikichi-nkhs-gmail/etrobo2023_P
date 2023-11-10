621e859e17e96f3b400c5e72a76f6fbbdecba5de
riku-0604 <rk2019get@gmail.com>
2023-11-10 21:26:51 +0900

    update4

--- section/SpeedSectionManager.h
+++ section/SpeedSectionManager.h
@@ -2,7 +2,7 @@
 #define _SPEED_SECTION_MANAGER_H_
 #include "SectionManager.h"
 
-#define SC_PATARN 1   //ショートカットパターンの選択
+#define SC_PATARN 3   //ショートカットパターンの選択
 
 class SpeedSectionManager : public SectionManager {
     public:
@@ -16,12 +16,17 @@ class SpeedSectionManager : public SectionManager {
         SecParam spara [100] = {
             
             #if SC_PATARN == 0 //safety parameter
+                {Section::WALKER,{0, 0},Section::TIME,{1},Section::NONE,{}},
                 {Section::DEFOLTARMCHANGE,{-15},Section::TIME,{3},Section::NONE,{}},
-                {Section::TRACER,{70, 0,  30, 10, 12,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{285},Section::NONE,{}},
-                {Section::TRACER,{40, 0,  33, 85, 16.9,10,0,LineTracer::LEFTEDGE},Section::LENGTH,{45},Section::NONE,{}},
-                {Section::TRACER,{70, 0,  30, 10, 12,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{165},Section::NONE,{}},
-                {Section::TRACER,{40, 0,  33, 85, 16.9,10,0,LineTracer::LEFTEDGE},Section::LENGTH,{55},Section::NONE,{}},
-                {Section::TRACER,{60, 0,  30, 10, 12,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{7},Section::NONE,{}},//LAP
+                {Section::TRACER,{75, 0,  49, 10, 12,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{294},Section::NONE,{}},
+                //{Section::WALKER,{0, 0},Section::TIME,{150},Section::NONE,{}},
+                {Section::TRACER,{50, 0,  40, 20, 10,18,0,LineTracer::LEFTEDGE},Section::LENGTH,{35},Section::NONE,{}},
+                //{Section::WALKER,{0, 0},Section::TIME,{150},Section::NONE,{}},
+                {Section::TRACER,{75, 0,  49, 10, 12,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{175},Section::NONE,{}},
+                //{Section::WALKER,{0, 0},Section::TIME,{150},Section::NONE,{}},
+                {Section::TRACER,{50, 0,  40, 20, 10,18,0,LineTracer::LEFTEDGE},Section::LENGTH,{35},Section::NONE,{}},
+                //{Section::WALKER,{0, 0},Section::TIME,{150},Section::NONE,{}},
+                {Section::TRACER,{75, 0,  49, 10, 12,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{20},Section::NONE,{}},//LAP
             
             #endif
             
@@ -29,7 +34,7 @@ class SpeedSectionManager : public SectionManager {
                 {Section::WALKER,{0, 0},Section::TIME,{1},Section::NONE,{}},
                 {Section::DEFOLTARMCHANGE,{-15},Section::TIME,{3},Section::NONE,{}},
                 //{Section::TRACER,{50, 0,  48, 10, 13,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{13},Section::NONE,{}},
-                {Section::TRACER,{80, 0,  40, 20, 12,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{275},Section::NONE,{}},
+                {Section::TRACER,{80, 0,  40, 20, 12,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{273},Section::NONE,{}},
                 //{Section::WALKER,{0, 0},Section::LENGTH,{150},Section::NONE,{}},
                 {Section::VIRTUALCURVE,{-35,80,13,1.5,0.7},Section::LENGTH,{54},Section::NONE,{}},
                 
@@ -68,6 +73,26 @@ class SpeedSectionManager : public SectionManager {
             
             #endif
             
+            #if SC_PATARN == 3
+                {Section::WALKER,{0, 0},Section::TIME,{1},Section::NONE,{}},
+                {Section::DEFOLTARMCHANGE,{-15},Section::TIME,{3},Section::NONE,{}},
+                //{Section::TRACER,{50, 0,  48, 10, 13,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{13},Section::NONE,{}},
+                {Section::TRACER,{80, 0,  40, 20, 12,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{260},Section::NONE,{}},
+                //{Section::WALKER,{0, 0},Section::LENGTH,{150},Section::NONE,{}},
+                {Section::VIRTUALCURVE,{-35,80,13,1.5,0.7},Section::LENGTH,{40},Section::NONE,{}},
+                
+                
+                {Section::VIRTUALSTRAIGHT,{0,100,30,10,3.45},Section::LENGTH,{20},Section::NONE,{}},
+                {Section::VIRTUALSTRAIGHT,{0,80,15,3,0.7},Section::BRIGHTNESS,{-0.2,0},Section::NONE,{}},
+                
+                
+                {Section::TRACER,{100, 0,  49, 10, 12,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{105},Section::NONE,{}},
+                //{Section::TRACER,{0, 0, 0, 0, 0,0,0,LineTracer::LEFTEDGE},Section::TIME,{100},Section::NONE,{}},
+                {Section::TRACER,{100, 0,  49, 10, 12,0,0,LineTracer::LEFTEDGE},Section::BRIGHTNESS,{0.4,1},Section::NONE,{}},
+                {Section::VIRTUALCURVE,{-15,80,13,1.5,0.7},Section::LENGTH,{24},Section::NONE,{}},
+                {Section::TRACER,{100, 0,  49, 10, 12,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{24},Section::NONE,{}},
+            
+            #endif
             
             {Section::END,{},Section::ENDJ,{}},
         };
