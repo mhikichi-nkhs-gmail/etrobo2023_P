84b84c48723759fcd6071edc02135c6ab2a64e2f
sato-nkhshc411033 <nkhshc411033@g.neec.ac.jp>
2023-11-12 18:15:29 +0900

    Update HackEv3.h

--- device/HackEv3.h
+++ device/HackEv3.h
@@ -1,9 +1,9 @@
 #ifndef __HACKEV3_H__
 #define __HACKEV3_H__
 
-#define SC_PATARN 3   //ã‚·ãƒ§ãƒ¼ãƒˆã‚«ãƒƒãƒˆãƒ‘ã‚¿ãƒ¼ãƒ³ã®é¸æŠž
+#define SC_PATARN 3  //ã‚·ãƒ§ãƒ¼ãƒˆã‚«ãƒ?ãƒˆãƒ‘ã‚¿ãƒ¼ãƒ³ã®é¸æŠ?
 
-#define PATARN 5   //ï¿½uï¿½ï¿½ï¿½bï¿½Nï¿½Ì”zï¿½uï¿½pï¿½^ï¿½[ï¿½ï¿½ï¿½É‚ï¿½ï¿½ï¿½Ä•Ï‚ï¿½ï¿½ï¿½
+#define PATARN 5   //?¿½u?¿½?¿½?¿½b?¿½N?¿½Ì”z?¿½u?¿½p?¿½^?¿½[?¿½?¿½?¿½É‚ï¿½?¿½?¿½Ä•Ï‚ï¿½?¿½?¿½
 
 const double D_RIGHT=10.00;
 const double D_LEFT=10.0;
